834734c8a38529a60c1a8457fd119578ba4cade1
heschroots <hschroots@ucdavis.edu>
2017-11-09 11:07:10 -0800

    Add array fucntionality with test cases

--- assignment5/arrstats.c
+++ assignment5/arrstats.c
@@ -2,65 +2,105 @@
 
 const int ARRAY_SIZE = 20;
 
-double arrmin(double array[])
+double arrmin(double array[], int size)
 {
 	double min_val = array[0];
-	for( int i = 1; i < ARRAY_SIZE; i++)
-	{
+	for( int i = 1; i < size; i++) {
 		if(array[i] < min_val)
 			min_val = array[i];
 	}
 	return min_val;
 }
 
-double arrmax(double array[])
+double arrmax(double array[], int size)
 {
 
 	double max_val = array[0];
-	for( int i = 1; i < ARRAY_SIZE; i++)
-	{
-		if(array[i] > min_val)
+	for( int i = 1; i < size; i++ ) {
+		if( array[i] > max_val )
 			max_val = array[i];
 	}
 	return max_val;
 }
 
-double arraver(double array[])
+double arraver(double array[], int size)
 {
 
 	double sum = 0;
-	for(int i=0; i < ARRAY_SIZE; i++)
+	for(int i=0; i < size; i++)
 		sum+=array[i];
-	return sum/ARRAY_SIZE;
+	return sum/(double)size;
 
 }
 
-void arrstats(double array[])
+void arrstats(double array[], int size)
 {
-	double min_val = arrmin(array);
-	double max_val = arrmax(array);
-	double avr_val = arraver(array);
+	double min_val = arrmin(array, size);
+	double max_val = arrmax(array, size);
+	double avr_val = arraver(array, size);
 
 	printf("Array min: %lf\n", min_val);
 	printf("Array max: %lf\n", max_val);
 	printf("Array average: %lf\n", avr_val);
 }
 
+void fillarray(double array[], int size)
+{
+	for( int i = 0; i< size; i++ )
+		array[i] = (double)i;
+}
+
+void testarrstats()
+{
+	double test_array[10];
+
+	double expected_min = 0.0;
+	double expected_max = 9.0;
+	double expected_avr = 4.5;
+
+	fillarray(test_array, 10);
+	printf("-------Testing array stats----------\n");
+	
+	double min_val = arrmin(test_array, 10);
+	double max_val = arrmax(test_array, 10);
+	double avr_val = arraver(test_array, 10);
+	
+	if( min_val != expected_min )
+		printf("ERROR: array min test failed\n");
+	else
+		printf("Array min test PASS\n");
+	if( max_val != expected_max )
+		printf("ERROR: array max test failed\n");
+	else
+		printf("Array max test PASS\n");
+	if( avr_val != expected_avr )
+		printf("ERROR: array average test failed\n");
+	else
+		printf("Array average test PASS\n");
+	printf("-------Test Complete----------\n\n\n");
+}
+
 int main(void)
 {
+	//testarrstats();
+
 	double num_array[ARRAY_SIZE];
 	
 	printf("Please enter up to %d double floats\n", ARRAY_SIZE);
 
-
-	int input_idx = 0;
-	while(scanf("%lf",num_array[input_idx]))
+	/*int input_idx = 0;
+	while( scanf("%lf...", num_array) )
 	{
+		printf("blue\n");
 		input_idx++;
 		if(input_idx >= ARRAY_SIZE)
 			break;
 	}
-	arrstats(num_array);
+	*/
+	scanf("%lf", num_array);
+	for(int i=0; i < ARRAY_SIZE; i++)
+		printf("[%d] = %lf\n", i, num_array[i]);
+	arrstats(num_array, ARRAY_SIZE);
 
 	return 0;
 }
